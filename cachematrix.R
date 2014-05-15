f4d534aa78864302f1f476cfc652b8f789e7d366
olivervandersluis <olivervandersluis@users.noreply.github.com>
2014-05-15 15:01:59 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,9 @@
-## 
+## the functions below 
 ## functions do
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix)
+makeCacheMatrix <- function(x)
 {
     #set the default values for the matrix and it's inverse
     CacheinverseMatrix <<- NULL
@@ -26,18 +26,24 @@ makeCacheMatrix <- function(x = matrix)
 
 
 
-## This function is called to 
+## This function is called to find the inverse of a matrix.
+## if the inverse has already been computed (the parameter 'matrix' is identical to the cached matrix)
+## The inverse matrix could also be found in the cache
+## if both matrices are different, the inverse is computed and stored in the cache
 
-cacheSolve <- function(x,...)
-{
-        ## Return a matrix that is the inverse of 'x'
-        ## if the function is called by default the inverse is set to NULL and the variable matrix is set to the parameter x
+## storing in the cache is done by the funtion "makeCacheMatrix" above.
+
+cacheSolve <- function(x = matrix)
+{   
+    ## if the function is called by default the inverse is set to NULL and the variable matrix is set to the parameter x
         
     matrix <- x
     inverse <- NULL
     
+    ## in order to call the functions to get/set the matrix and it's inverse, the main funtion is stored in a variable
     mtrxfn <- makeCacheMatrix()
     
+    ## get the chached matrix
     GetMatrix <- mtrxfn$getMatrix()
     
     if(identical(GetMatrix, x))
