d9ad8620be68ecb3de038e1a92633c121ee908c2
olivervandersluis <olivervandersluis@users.noreply.github.com>
2014-05-16 16:43:50 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,11 +3,13 @@
 
 ## this functions contains 4 functions which get and set the matrix and its inverse in the cache
 ## The function "makeCacheMatrix" (and its functions) is called by the cacheSolve in order to retrieve the cached
+##matrix inverse, or  to set the matrix and its inverse
 makeCacheMatrix <- function(x = matrix)
 {
-
+    ##this function sets the matrix to the cache
     setMatrix <- function(x) Cachematrix <<- x
     
+    ##this function gets the matrix from the cache, if it exists else it returns NULL
     getMatrix <- function()
     {
         if (exists("Cachematrix"))
@@ -20,11 +22,13 @@ makeCacheMatrix <- function(x = matrix)
         }
     }  
     
+    ##this function sets the inverse matrix in the cache
     setInverse <- function(inverse)  CacheinverseMatrix <<- inverse
                                      
-    
+     ##this function gets the inverse matrix from the cache
     getInverse <- function()  CacheinverseMatrix
 
+    ## this list is created in order to call the functions within "makeCacheMatrix"
     list(setMatrix = setMatrix, getMatrix = getMatrix,
          setInverse = setInverse,
          getInverse = getInverse)
